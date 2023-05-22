22437632353092328ff6bc22ab4d7f6d1993730d
procode3 <wr.t8r1@gmail.com>
2023-05-21 17:37:51 -0700

    TodoList React App

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import Todolist from './components/Todolist';
+import NewTodo from './components/NewTodo';
+import { Todo } from './todo.model' 
+
+const App: React.FC = () => {
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+  const todoAddHandler = (text: string) => {
+    setTodos(prevTodos => [
+      ...prevTodos,
+      {id: Math.random().toString(), text: text}
+    ]);
+  };
+  
+  const todoDeletehandler = (todoId: string) => {
+    setTodos(prevTodos => {
+      return prevTodos.filter(todo => todo.id !== todoId);
+    } );
+  };
+  return <div className='App'>
+    <NewTodo onAddTodo={todoAddHandler} />
+    <Todolist items={todos} onDeleteTodo={todoDeletehandler}/>
+  </div>;
+};
 
 export default App;
---
+++ src/components/Todolist.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+import './TodoList.css';
+
+interface TodolistProps {
+    items:{id: string, text: string}[];
+    onDeleteTodo: (id: string) => void;
+};
+
+
+const Todolist: React.FC<TodolistProps> = props => {
+   
+    return <ul>
+        {props.items.map(todo =>
+            <li key={todo.id}>
+            <span>{todo.text}</span>
+            <button onClick={props.onDeleteTodo.bind(null, todo.id)}>DELETE</button>
+            </li>)}
+        
+    </ul>
+    
+};
+
+
+export default Todolist;
